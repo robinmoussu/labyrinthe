a5a4c5a8064ab7b107525cdc8d5541a925f75f4d
robin moussu <robin.moussu+git@gmail.com>
2014-08-11 20:54:41 +0000

    in progress… work on square visibilitee

--- labyrinthe.c
+++ labyrinthe.c
@@ -1,99 +1,116 @@
 #include <Array>
 #include <Pair>
 
-Class Case {
+class Coordonnee {
+    Coordonnee(int x, int y);
+    std::Array<Coordonnee, 4> voisins;
+    std::Array<Coordonnee, 4> diagonales;
+    std::Array<Coordonnee, 4> voisins_loin;
+    Coordonnee operator+(Coordonnee position);
+    int x() const {return position.first;}
+    int y() const {return position.second;}
     protected:
-    int visibilitee;
+    std::pair<int> position;
+}
+
+std::Array<Coordonnee, 4> Coordonnee::voisins() {
+    std::Array<Coordonnee, 4> voisins;
+    voisins[0] = position + Coordonnee(0,1);
+    voisins[1] = position + Coordonnee(1,0);
+    voisins[2] = position + Coordonnee(0,-1);
+    voisins[3] = position + Coordonnee(-1,0);
+    return voisins;
+}
+
+std::Array<Coordonnee, 4> Coordonnee::diagonales() {
+    std::Array<Coordonnee, 4> voisins;
+    voisins[0] = position + Coordonnee(1,1);
+    voisins[1] = position + Coordonnee(1,-1);
+    voisins[2] = position + Coordonnee(-1,-1);
+    voisins[3] = position + Coordonnee(-1,1);
+    return voisins;
+}
+
+std::Array<Coordonnee, 4> Coordonnee::voisins_loin() {
+    std::Array<Coordonnee, 4> voisins;
+    voisins[0] = position + Coordonnee(0,2);
+    voisins[2] = position + Coordonnee(2,0);
+    voisins[2] = position + Coordonnee(0,-2);
+    voisins[3] = position + Coordonnee(-2,0);
+    return voisins;
+}
+
+Coordonnee operator+(Coordonnee position) {
+    return Coordonnee(this->x() + position.x(), this->y() + position.y());
+}
+
+class Fourmi {
+    Coordonnee position() {return position;}
     protected:
-    static const int BASE_POINT         = 90;
-    static const int POINT_VOISINS      = 8;
-    static const int POINT_DIAGONALE    = 7;
-    static const int POINT_VOISINS_LOIN = 6;
+    Coordonnee position;
 }
 
 class Map {
     void deplacer_fourmi();
+    Box *const operator[] (Coordonnee const coordonnee) const;
+    void update_visibilitee(Coordonnee const coordonnee);
     protected:
-    int nb_total_point();
+    int nb_total_point() const {return nb_total_point;}
+
+    Class Box {
+        bool occupee() const {return occupee;}
+        bool visibilitee() const {return visibilitee;}
+        protected:
+        bool occupee = false;
+        int visibilitee = 0;
+        protected:
+        static const int BASE_POINT         = 90;
+        static const int POINT_VOISINS      = 8;
+        static const int POINT_DIAGONALE    = 7;
+        static const int POINT_VOISINS_LOIN = 6;
+    }
+
     protected:
-    std::vector<bool> map;
+    int width;
+    int hight;
+    int nb_total_point = 0;
+    std::vector<Box> map;
     std::vector<Fourmi> fourmis;
 }
 
 void Map::deplacer_fourmi() {
     // TODO: remettre ça au propre
     map[rand() % nb_total_point()] = nouvelle position;
+
+    // mise à jour des cases
+    // la zone contenant les cases à mettre à jour est un rectangle de 3×4 cases
+    // On compte le nombre de point 
 }
 
-int nb_total_point() {
-    int nb_total_point = BASE_POINT;
-    for (fourmi: fourmis) {
-        for (voisin: fourmi.voisins()) {
+Box *const Map::operator[] (Coordonnee const coordonnee) const {
+    return map[coordonnee.x*width + coordonnee.y]
+}
+
+void Map::update_visibilitee(Coordonnee const coordonnee) {
+    const Box box = self->[coordonnee].occupee;
+    if (box) {
+        visibilitee = 0;
+    } else {
+        visibilitee = BASE_POINT;
+        for (voisin: coordonnee.voisins()) {
             if (map.case_occupee(voisin.position())) {
                 nb_total_point -= POINT_VOISINS;
             }
         }
-        for (diagonale: fourmi.diagonales()) {
+        for (diagonale: coordonnee.diagonales()) {
             if (map.case_occupee(diagonale.position())) {
                 nb_total_point -= POINT_DIAGONALES;
             }
         }
-        for (voisin_loin: fourmi.voisins_loin()) {
+        for (voisin_loin: coordonnee.voisins_loin()) {
             if (map.case_occupee(voisin_loin.position())) {
                 nb_total_point -= POINT_VOISINS_LOINS;
             }
         }
     }
-    return nb_total_point;
 }
-
-class Position {
-    Position(int x, int y);
-    std::Array<Pos, 4> voisins;
-    std::Array<Pos, 4> diagonales;
-    std::Array<Pos, 4> voisins_loin;
-    Position operator+(Position position);
-    int x() {return position.first;}
-    int y() {return position.second;}
-    protected:
-    std::pair<int> position;
-}
-
-std::Array<Position, 4> voisins() {
-    std::Array<Position, 4> voisins;
-    voisins[0] = position + Position(0,1);
-    voisins[1] = position + Position(1,0);
-    voisins[2] = position + Position(0,-1);
-    voisins[3] = position + Position(-1,0);
-    return voisins;
-}
-
-std::Array<Position, 4> diagonales() {
-    std::Array<Position, 4> voisins;
-    voisins[0] = position + Position(1,1);
-    voisins[1] = position + Position(1,-1);
-    voisins[2] = position + Position(-1,-1);
-    voisins[3] = position + Position(-1,1);
-    return voisins;
-}
-
-std::Array<Position, 4> voisins_loin() {
-    std::Array<Position, 4> voisins;
-    voisins[0] = position + Position(0,2);
-    voisins[2] = position + Position(2,0);
-    voisins[2] = position + Position(0,-2);
-    voisins[3] = position + Position(-2,0);
-    return voisins;
-}
-
-Position operator+(Position position) {
-    return Position(this->x() + position.x(), this->y() + position.y());
-}
-
-class Fourmi {
-    std::pair< std::Array<Position, 4>, std::Array<int, 4> > Fourmi::case_visible(Fourmi fourmi) {
-    Position position() {return position;}
-    protected:
-    Position position;
-}
-
